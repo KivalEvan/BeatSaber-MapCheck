6e2cbb0f5fa6f80d8e49e9112e491dfe9a7f5f47
Kival Evan <mnabawiazizul@gmail.com>
2021-02-09 07:59:11 +0800

    wiki and changelog link

--- scripts/global.js
+++ scripts/global.js
@@ -2,7 +2,7 @@
     is it really a script if it's just holding a global variable? */
 
 const watermark = 'Kival Evan#5480';
-const version = 'v1.3.3';
+const version = 'v1.3.4';
 
 const flag = {
     loading: false,
