4c2c663824d542c658fbde92f40b899afbcfc37b
Kival <5645391+KivalEvan@users.noreply.github.com>
2022-09-12 17:21:09 +0800

    Updated progression check.

--- src/ts/tools/general/progression.ts
+++ src/ts/tools/general/progression.ts
@@ -4,6 +4,7 @@ import SavedData from '../../savedData';
 import * as swing from '../../analyzers/swing';
 import { printResult } from '../helpers';
 import UICheckbox from '../../ui/helpers/checkbox';
+import { DifficultyRename } from '../../beatmap/shared/difficulty';
 
 const name = 'Difficulty Progression';
 const description = 'For ranking purpose, check difficuly progression to fit rankability criteria.';
@@ -39,30 +40,52 @@ function run(map: ToolArgs) {
     const filteredSPS = SavedData.beatmapDifficulty
         .map((d) => d.swingAnalysis)
         .filter((a) => a.characteristic === 'Standard')
-        .sort((a, b) => a.total.count - b.total.count);
+        .sort((a, b) => a.total.average - b.total.average)
+        .reverse();
     if (!filteredSPS.length) {
         tool.output.html = null;
         return;
     }
-    const minSPS = audioDuration < 120 ? 3.2 : audioDuration < 240 ? 4.2 : 5.2;
+    const minSPS = Math.max(
+        audioDuration < 120 ? 3.2 : audioDuration < 240 ? 4.2 : 5.2,
+        round(swing.getSPSHighest(filteredSPS) * 0.4, 2),
+    );
 
     const htmlResult: HTMLElement[] = [];
-    if (
-        audioDuration < 240 &&
-        swing.getSPSLowest(filteredSPS) > minSPS &&
-        swing.calcSPSTotalPercDrop(filteredSPS) < 60
-    ) {
+    if (audioDuration < 360 && swing.getSPSLowest(filteredSPS) > minSPS) {
         htmlResult.push(
-            printResult(`Minimum SPS not met (<${minSPS})`, `lowest is ${round(swing.getSPSLowest(filteredSPS), 2)}`),
+            printResult(
+                `Minimum SPS not met (<${minSPS})`,
+                `lowest SPS is ${round(swing.getSPSLowest(filteredSPS), 2)}, ${round(
+                    swing.calcSPSTotalPercDrop(filteredSPS),
+                    2,
+                )}% drop from highest SPS (${round(swing.getSPSHighest(filteredSPS), 2)})`,
+            ),
         );
     }
     const progMax = swing.getProgressionMax(filteredSPS, minSPS);
     const progMin = swing.getProgressionMin(filteredSPS, minSPS);
     if (progMax && audioDuration < 360) {
-        htmlResult.push(printResult('Violates progression', `${progMax.difficulty} exceeded 40% SPS drop`));
+        htmlResult.push(
+            printResult(
+                'Violates progression',
+                `${DifficultyRename[progMax.result.difficulty]} exceeded 40% SPS drop from ${round(
+                    progMax.result.total.average,
+                    2,
+                )}, acceptable range (${round(progMax.min, 2)}-${round(progMax.max, 2)})`,
+            ),
+        );
     }
     if (progMin && audioDuration < 360) {
-        htmlResult.push(printResult('Violates progression', `${progMin.difficulty} has less than 10% SPS drop`));
+        htmlResult.push(
+            printResult(
+                'Violates progression',
+                `${DifficultyRename[progMin.result.difficulty]} has less than 10% SPS drop from ${round(
+                    progMin.result.total.average,
+                    2,
+                )}, acceptable range (${round(progMin.min, 2)}-${round(progMin.max, 2)})`,
+            ),
+        );
     }
 
     if (htmlResult.length) {
