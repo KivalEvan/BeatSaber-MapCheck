c6a1e2784283e86d6648ef1ec244cbae310a17fc
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2024-08-31 11:18:11 +0800

    Begone ambiguous module problem

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,4 @@
-import { builtinModules } from 'module';
-
 export default {
    css: { preprocessorOptions: { scss: { charset: false } } },
-   base: '/BeatSaber-MapCheck/',
-   build: { // FIXME: i need to fix shims on bsmap
-      rollupOptions: {
-         external: [...builtinModules, /^node:/]
-      }
-   }
+   base: '/BeatSaber-MapCheck/'
 };
