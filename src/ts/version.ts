8ad8d957e95ea045389e1b1cf74fb1c57ad35c3e
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2024-10-12 10:34:39 +0800

    Bump

--- src/ts/version.ts
+++ src/ts/version.ts
@@ -3,7 +3,7 @@ import { version } from '../../package.json';
 const watermark: string = 'Kival Evan';
 
 export default new (class Version {
-   private _version: string = version + '-beta-b';
+   private _version: string = version + '-beta-c';
    private _wm: string = watermark;
 
    get value(): string {
