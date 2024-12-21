1d70517ace789313d2911e8a39c3ba3199bafcf2
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2024-12-21 21:49:00 +0800

    Add NJS events to stats

--- src/ts/ui/stats/settings.ts
+++ src/ts/ui/stats/settings.ts
@@ -9,6 +9,15 @@ const htmlSettingsVersion = document.getElementById(
 const htmlSettingsNjs = document.getElementById(
    'stats__table-settings-njs',
 ) as HTMLTableCellElement;
+const htmlSettingsNjsMin = document.getElementById(
+   'stats__table-settings-njs-min',
+) as HTMLTableCellElement;
+const htmlSettingsNjsMax = document.getElementById(
+   'stats__table-settings-njs-max',
+) as HTMLTableCellElement;
+const htmlSettingsNjsCount = document.getElementById(
+   'stats__table-settings-njs-count',
+) as HTMLTableCellElement;
 const htmlSettingsSdm = document.getElementById(
    'stats__table-settings-sdm',
 ) as HTMLTableCellElement;
@@ -27,10 +36,14 @@ export function updateSettingsTable(
       beatmapItem.settings.njs || NoteJumpSpeed.FallbackNJS[beatmapItem.settings.difficulty],
       beatmapItem.settings.njsOffset,
    );
+   const njsEvents = beatmapItem.data.njsEvents.toSorted((a, b) => a.value - b.value);
 
    htmlSettingsVersion.textContent =
       (beatmapItem.rawData as any).version || (beatmapItem.rawData as any)._version || 'N/A';
    htmlSettingsNjs.textContent = round(njs.value, 3).toString();
+   htmlSettingsNjsMax.textContent = round(njs.value + (njsEvents.at(0)?.value || 0), 3).toString();
+   htmlSettingsNjsMin.textContent = round(njs.value + (njsEvents.at(-1)?.value || 0), 3).toString();
+   htmlSettingsNjsCount.textContent = round(njsEvents.length, 3).toString();
    htmlSettingsSdm.textContent = round(njs.offset, 3).toString();
    htmlSettingsHjd.textContent = round(njs.hjd, 3).toString();
    htmlSettingsJd.textContent = round(njs.jd, 3).toString();
