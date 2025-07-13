1eae37603d6e9670f6873861561a6eb9c67de4a0
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2025-07-13 18:10:12 +0800

    Somehow missed this

--- src/ts/ui/checks/output.ts
+++ src/ts/ui/checks/output.ts
@@ -1,31 +1,31 @@
 import { round, secToMmss, secToMmssms } from 'bsmap/utils';
 import * as types from 'bsmap/types';
 import { Settings } from '../../settings';
-import { OutputSymbol } from '../../types/checks/check';
+import { OutputStatus } from '../../types/checks/check';
 
-function addLabel(str: string, symbol?: OutputSymbol): string {
+function addLabel(str: string, symbol?: OutputStatus): string {
    switch (symbol) {
-      case 'rank':
+      case OutputStatus.RANK:
          str = '<span title="Ranking: for rankability reason."> 🚧 </span>' + str;
          break;
-      case 'error':
+      case OutputStatus.ERROR:
          str =
             '<span title="Error: should be fixed unless you know what you are doing."> ❌ </span>' +
             str;
          break;
-      case 'warning':
+      case OutputStatus.WARNING:
          str =
             '<span title="Warning: not necessarily needed to be fixed, worth considering."> ❗ </span>' +
             str;
          break;
-      case 'info':
+      case OutputStatus.INFO:
          str = '<span title="Info: no action necessary, take note."> ⚠️ </span>' + str;
          break;
    }
    return str;
 }
 
-export function printResult(label: string, text?: string, symbol?: OutputSymbol) {
+export function printResult(label: string, text?: string, symbol?: OutputStatus) {
    const htmlContainer = document.createElement('div');
 
    label = addLabel(label, symbol);
@@ -46,7 +46,7 @@ function deduplicateFilter<T extends types.wrapper.IWrapBaseObject>(obj: T, i: n
 export function printResultTime(
    label: string,
    timeAry: types.wrapper.IWrapBaseObject[],
-   symbol?: OutputSymbol,
+   symbol?: OutputStatus,
 ) {
    const htmlContainer = document.createElement('div');
 
